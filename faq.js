b2f9c3312678590a3da8026db3b5a53e5c9abea5
Otaconned <144296987+Otaconned@users.noreply.github.com>
2024-09-30 19:14:27 -0400

    Update faq.js

--- faq.js
+++ faq.js
@@ -1,7 +1,7 @@
-const content = document.getElementById('textbox');
-const button = document.getElementByID('expand');
+const content = document.getElementById('content');
+const button = document.getElementById('more-btn');
 
-button.addEventListener(click, function()){
-    const more = content.classList.toggle('more');
-    button.textContent = more ? 'Read Less' : 'Read More';
+button.addEventListener('click', function(){
+    const expanded = content.classList.toggle('expanded');
+    button.textContent = expanded ? 'Read Less' : 'Read More';
 });
