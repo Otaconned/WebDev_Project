e732fa55d027aaec38d0c71b079348a36ac42014
Otaconned <144296987+Otaconned@users.noreply.github.com>
2024-10-25 14:29:25 -0400

    Create report.js

---
+++ report.js
@@ -0,0 +1,16 @@
+let emailInput = document.getElementById('');
+let issueInput = document.getElementById('');
+let isValid = true;
+
+document.getElementById('report').addEventListener('submit', function(event){
+  event.preventDefault();
+
+  let display = document.getElementById('display');
+  display.style.display = 'block';
+
+  document.getElementById('report').reset();
+});
+
+document.getElementById('display').addEventListener('click', function(){
+  this.style.display = 'none';
+});
