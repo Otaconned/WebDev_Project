6d479045ee24a4b95797fb678385ccacd793c745
Otaconned <144296987+Otaconned@users.noreply.github.com>
2024-10-25 14:01:42 -0400

    Create faq.js

---
+++ faq.js
@@ -0,0 +1,7 @@
+const content = document.getElementById('content');
+const button = document.getElementById('more-btn');
+
+button.addEventListener('click', function(){
+    const expanded = content.classList.toggle('expanded');
+    button.textContent = expanded ? 'Read Less' : 'Read More';
+});
