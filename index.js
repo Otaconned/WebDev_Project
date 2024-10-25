a3dbabf9a8c2529f71ac79c55180d1f68fadbd99
Otaconned <144296987+Otaconned@users.noreply.github.com>
2024-10-25 14:29:00 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+let slidesArray = 0;
+imageCarousel();
+
+function imageCarousel(){
+  let slide = document.getElementsByClassName("slide");
+  
+  let (let i = 0; i < s.length; i++){
+    slide[i].style.display = "none";
+  }
+  slidesArray++
+  if (slidesArray > s.length){slidesArray = 1}
+  slide[slidesArray-1].style.display = "block";
+  setTimeout(imageCarousel, 2000);
+}
