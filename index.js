e599f822faaaa0e660833068eb97af6f51431520
Otaconned <144296987+Otaconned@users.noreply.github.com>
2024-10-31 12:17:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,14 +7,20 @@ function carousel(){
   let slides = document.getElementsByClassName("slide");
   
   let (let i = 0; i < slides.length; i++){
-    slide[i].style.display = "none";
+    slides[i].style.display = "none";
   }
   
   slidesArray++
   if (slideIndex > slides.length){
     slidesArray = 1
   }
-  
+
+  let (let i = 0; i < slides.length; i++){
+    slides[i].classList.remove = ("active");
+  }
+
+  slide[slideIndex-1].classList.add = ("active");
   slide[slideIndex-1].style.display = "block";
   setTimeout(carousel, 2000);
 }
+
