7308a49c139b241b00e525791bde5c2188830182
Otaconned <144296987+Otaconned@users.noreply.github.com>
2024-11-01 19:34:12 -0400

    Update faq.js

--- faq.js
+++ faq.js
@@ -1,7 +1,9 @@
-const content = document.getElementById('content');
-const button = document.getElementById('more-btn');
+document.querySelectorAll('.faq-item').forEach(item => {
+    const content = item.querySelector('content');
+    const button = item.querySelector('expand');
 
-button.addEventListener('click', function(){
-    const expanded = content.classList.toggle('expanded');
-    button.textContent = expanded ? 'Read Less' : 'Read More';
-}); 
+    button.addEventListener('click', function(){
+        const expanded = content.classList.toggle('expanded');
+        button.textContent = content.classList.contains('expanded') ? 'Read Less' : 'Read More';
+    }); 
+}
