80b56facc100c20cd1d144ea5ddde39af9517c7e
Otaconned <144296987+Otaconned@users.noreply.github.com>
2024-11-01 11:28:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 'use strict';
 
-const slides = Array.from(document.querySelectorAll('.slide'));
-let currentIndex = 0;
-
+/*Function to autoplay a carousel of images, using references to MD3 Schools & in-class carousel*/
 function carousel(){
+  const slides = Array.from(document.querySelectorAll('.slide'));
+  let currentIndex = 0;
+  
   slides[currentIndex].classList.remove('active');
   currentIndex = (currentIndex + 1) % slides.length;
   slides[currentIndex].classList.add('active');
 }
 
 setInterval(carousel, 2000);
+
+/*Function to allow the user to increase/decrease page text with buttons*/
+function increaseFont(increaseBy){
+  const content = document.querySelectorAll('p'); 
+
+  content.forEach((p) => {
+    let style = window.getComputedStyle(p).getPropertyValue('font-size');
+    let currentSize = parseFloat(style);
+    p.style.fontSize = (currentSize + increaseBy) + 'px';
+  });
+}
+function decreaseFont(decreaseBy){
+  const content = document.querySelectorAll('p'); 
+
+  content.forEach((p) => {
+    let style = window.getComputedStyle(p).getPropertyValue('font-size');
+    let currentSize = parseFloat(style);
+    p.style.fontSize = (currentSize - decreaseBy) + 'px';
+  });
+}
