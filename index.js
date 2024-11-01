9080fe1b35144b18e5d3d6d29ba0e57e755b9716
Otaconned <144296987+Otaconned@users.noreply.github.com>
2024-10-31 21:41:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10,3 +10,21 @@ function carousel(){
 }
 
 setInterval(carousel, 2000);
+
+const increaseSize = document.getElementById('increase-size');
+const decreaseSize = document.getElementById('decrease-size');
+const text = document.querySelector("p");
+
+let currentSize = 16;
+
+increaseSize.addEventListener('click' ()=> {
+  currentSize += 2;
+  text.style.fontSize = currentSize;
+}
+
+increaseSize.addEventListener('click' ()=> {
+  if (currentSize > 10) {
+    currentSize -= 2;
+    text.style.fontSize = currentSize;
+  }
+}
