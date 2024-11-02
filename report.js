109e2731e0bbdc9fa0f903cc4be1ec6bb5b69d44
Otaconned <144296987+Otaconned@users.noreply.github.com>
2024-11-02 13:02:38 -0400

    Update report.js

--- report.js
+++ report.js
@@ -1,15 +1,25 @@
-let emailInput = document.getElementById('');
-let issueInput = document.getElementById('');
-let isValid = true;
+const form = document.getElementById('report');
+const emailInput = document.getElementById('email');
+const display = document.getElementById('display');
 
-document.getElementById('report').addEventListener('submit', function(event){
-  event.preventDefault();
+function validateEmail(emailInput) {
+  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return regex.test(String(emailInput).toLowerCase());
+}
 
+button.addEventListener('click', function() {
+  let emailInput = document.getElementById('email');
   let display = document.getElementById('display');
-  display.style.display = 'block';
 
-  document.getElementById('report').reset();
-});
+  display.style.display = 'block';
+  
+  if (validateEmail(email)) {
+    display.innerHTML = "Thank you for your input!"
+    document.getElementById('report').reset();
+  } else {
+    display.innerHTML = "Please use a valid email."
+  }
+}
 
 document.getElementById('display').addEventListener('click', function(){
   this.style.display = 'none';
