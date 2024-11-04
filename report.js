7b9564de473cab71395b91148ff24561a8f72bbb
Otaconned <144296987+Otaconned@users.noreply.github.com>
2024-11-03 21:44:02 -0500

    Update report.js

--- report.js
+++ report.js
@@ -1,29 +1,30 @@
-const form = document.getElementById('report');
-const emailInput = document.getElementById('email');
-const textInput = document.getElementById('issue');
-const display = document.getElementById('display');
+'use strict';
 
-function validateEmail(emailInput) {
-  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+function validateEmail() {
+  let emailInput = document.getElementById('email');
+  let regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
   return regex.test(String(emailInput).toLowerCase());
 }
 
-button.addEventListener('click', function() {
-  let emailInput = document.getElementById('email');
-  let display = document.getElementById('display');
-
-  display.style.display = 'block';
-  
-  if (validateEmail(email)) {
-    display.innerHTML = "Thank you for your input!"
-    document.getElementById('report').reset();
-  } else {
-    display.innerHTML = "Please use a valid email."
-  }
+function popUpMessage(message) {
+  popUp.textContent = message;
+  popUp.style.display = "block";
 }
 
-document.getElementById('display').addEventListener('click', function(){
-  this.style.display = 'none';
+submitButton.addEventListener("click", function () {
+  const form = document.getElementById('report');
+
+  if (validateEmail = true) {
+    popUpMessage("Thank you for your input!");
+    form.reset();
+  } else {
+    popUpMessage("Please enter a valid email address");
+  }
+});
+
+document.getElementById('popUp').addEventListener('click', function(){
+  popUp.style.display = 'none';
 }); 
 
 document.addEventListener("input", function() {
