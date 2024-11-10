65816103f6b2d124fcbf4976270f0d782b24fe21
Otaconned <144296987+Otaconned@users.noreply.github.com>
2024-11-09 22:58:36 -0500

    Update report.js

--- report.js
+++ report.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*function which tracks the user's input and adjusts the display to turn red when nearing the max*/
 function characterCounter(obj) {
   const counter = document.getElementById("counter");
   let maxChars = 280;
@@ -14,11 +15,12 @@ function characterCounter(obj) {
   }
 }
 
+/*Referenced from ZyBooks*/
 function validateForm() {
   const form = document.getElementById("report");
   const email = document.getElementById("email");
   const popUp = document.getElementById("popUp");
-  const regEx = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}$/;
+  const regEx = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}$/; /*Regex from a previous assignment*/
   
   if (!email.value || !regEx.test(email.value)) {
     popUp.textContent = "Please use a valid email address.";
@@ -30,6 +32,7 @@ function validateForm() {
   popUp.classList.remove("hide");
 }
 
+/*seperate mini-functions to ensure additional functionality; keeping the form from triggering a page re-load and allow the user to dismiss the popup via clicking.*/
 document.getElementById("submit").addEventListener("click", function(event) {
   validateForm();
   event.preventDefault();
