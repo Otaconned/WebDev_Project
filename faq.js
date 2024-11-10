aa64d2a23f9cf30901d3111394cb72939c82ae62
Otaconned <144296987+Otaconned@users.noreply.github.com>
2024-11-09 22:53:29 -0500

    Update faq.js

--- faq.js
+++ faq.js
@@ -1,10 +1,15 @@
+'use strict';
+
+/*declare the global variable since this is the only function*/
 let buttons = document.querySelectorAll('.expand');
 
+/*uses previous element's sibling referenced from MDN docs, since each button should be independent of one another (but attatched to their respective span function)*/
 for (let i = 0; i < buttons.length; i++){
     buttons[i].addEventListener('click', function(event){
         let span = event.target.previousElementSibling.querySelector('span');
-        span.classList.toggle('fade-in');
+        span.classList.toggle('fade-in'); /*function which impacts the animation style*/
 
+        /*in the future, I could probably combine these with a ? operator, but was having some trouble getting them to work when I did so.*/
         if (span.classList.contains('fade-in')){
             event.target.textContent = 'Read Less';
         } else {
