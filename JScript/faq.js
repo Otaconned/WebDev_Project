e9e96dced8e71c5217a470150da9b81b78579db2
Otaconned <144296987+Otaconned@users.noreply.github.com>
2024-12-08 10:08:54 -0500

    Delete JScript directory

--- JScript/faq.js
+++
@@ -1,7 +0,0 @@
-const content = document.getElementById('content');
-const button = document.getElementById('more-btn');
-
-button.addEventListener('click', function(){
-    const expanded = content.classList.toggle('expanded');
-    button.textContent = expanded ? 'Read Less' : 'Read More';
-});
